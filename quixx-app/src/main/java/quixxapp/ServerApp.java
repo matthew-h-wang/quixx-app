968eca99aab1d005e26b693b90a2cc1deff40eb1
Matthew <wang.matthew.h@gmail.com>
2020-05-23 11:37:51 -0400

    cleanup, launch props

---
+++ quixx-app/src/main/java/quixxapp/ServerApp.java
@@ -0,0 +1,22 @@
+package quixxapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class ServerApp {
+    public static void main(String[] args) {
+        //TODO: Add multigame handling, with codes.
+        
+
+    }
+
+    class GameRunner implements Runnable {
+        @Override
+        public void run() {
+            
+            
+        }
+    }
+
+}
